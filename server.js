846ee48dd09af5102f606a9713f7253284cd3734
Madeleine36 <67713754+Madeleine36@users.noreply.github.com>
2025-01-18 19:37:18 -0700

    Added alternate preprompts

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const port = 8080;
 const genAI = new GoogleGenerativeAI("AIzaSyB5ejtB_QpueazK_KIKxBNFCZ0Vu-Arc9c");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const preprompt = "Rephrase this text to be less emotionally intense, by about 3 points on a scale from 1 to 10. I will give you the inputs as a list of strings, please have the outputs be a list of strings as well:";
+const preprompt_nice = "Rephrase this text to be less emotionally intense, by about 3 points on a scale from 1 to 10. I will give you the inputs as a JSON list, please have the outputs be a JSON list as well:";
+const preprompt_mean = "Rephrase this text to be more emotionally intense and aggressive (it's okay to use profanity),, by about 3 points on a scale from 1 to 10. I will give you the inputs as a JSON list, please have the outputs be a JSON list as well:";
+
 
 app.use(cors());
 app.use(cors({
