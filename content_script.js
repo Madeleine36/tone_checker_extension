7a5c45e1602d3e7c82e4be02dae70c5861dc7317
Tyler Kerswell <tylerkerswell@gmail.com>
2025-01-18 17:20:09 -0800

    base req/resp

--- content_script.js
+++ content_script.js
@@ -1,5 +1,10 @@
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
+
+var totalreq = []
+
 for (let i=0; i < text.length; i++) {
+    totalreq.push(String(text[i].innerHTML));
+
     // my new code here
     // this is where we'd edit an individual chunk of text
     if (text[i].innerHTML.includes ('Tom Brady')) {
@@ -8,3 +13,25 @@ for (let i=0; i < text.length; i++) {
     text[i].innerHTML = text[i].innerHTML.replace('Brady', '6-time Super Bowl champion Tom Brady')
     }
 }
+
+
+
+fetch('http://localhost:8080/translate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+    body: JSON.stringify(totalreq),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((data) => {
+      console.log('Response from server: ', data);
+    })
+    .catch((error) => {
+      console.error('hey this is your Error:', error);
+    });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 const port = 8080;
@@ -8,8 +9,12 @@ const port = 8080;
 
 const genAI = new GoogleGenerativeAI("AIzaSyB5ejtB_QpueazK_KIKxBNFCZ0Vu-Arc9c");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-const preprompt = "Rephrase this text to be less emotionally intense, by about 3 points on a scale from 1 to 10:";
+const preprompt = "Rephrase this text to be less emotionally intense, by about 3 points on a scale from 1 to 10. I will give you the inputs as a JSON list, please have the outputs be a JSON list as well:";
 
+app.use(cors());
+app.use(cors({
+    origin: 'https://motherfuckingwebsite.com',
+  }));  
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -21,7 +26,7 @@ app.listen(port, () => {
 
 app.use(bodyParser.text());
 
-app.get('/hello', async (req, res) => {
+app.post('/translate', async (req, res) => {
     var prompt = preprompt + req.body;
 
     var result = await model.generateContent(prompt);
