8c96af3567c7502c1504e53b041b2659010096ed
Madeleine36 <67713754+Madeleine36@users.noreply.github.com>
2025-01-18 17:06:34 -0700

    added openai to content script (not working)

--- content_script.js
+++ content_script.js
@@ -1,3 +1,13 @@
+console.log("hello from content_script");
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI("AIzaSyB5ejtB_QpueazK_KIKxBNFCZ0Vu-Arc9c");
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Rephrase this text to be less emotionally intense, by about 3 points on a scale from 1 to 10: now that enough time has passed putting “the last scene” music in sonic 3 was extremely criminal due to being a targeted attack on my emotions";
+
+const result = model.generateContent(prompt);
+console.log(result.response.text());
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
 for (let i=0; i < text.length; i++) {
     // my new code here
